1ad0cbbf2b4f16ce009c9255ebce44550c076a7d
statisticallyfit <statisticallyfit@gmail.com>
2018-01-20 10:14:01 -0500

    starting custom functor prop

--- src/test/scala/functor/data/ADTs.scala
+++ src/test/scala/functor/data/ADTs.scala
@@ -6,3 +6,29 @@ import cats.{Eq, Monoid, Functor}
 
 
 case class Identity[A](value: A)
+
+object Identity {
+
+     implicit def functorIdentity[A] = new Functor[Identity[A]] {
+
+          def map[_, B](fa: Identity[A])(f: A => B): Identity[B] = {
+               Identity(f(fa.value))
+          }
+     }
+
+     implicit def eqIdentity[A : Eq] = new Eq[Identity[A]] {
+
+          def eqv(id1: Identity[A], id2: Identity[A]): Boolean = Eq[A].eqv(id1.value, id2.value)
+     }
+}
+
+
+// ------------------------------------------------------------------------------------------
+
+case class Temp(int: Int)
+
+// ------------------------------------------------------------------------------------------
+
+
+
+// ------------------------------------------------------------------------------------------
--- src/test/scala/functor/data/ArbitraryADTs.scala
+++ src/test/scala/functor/data/ArbitraryADTs.scala
@@ -1,8 +1,24 @@
 package functor.data
 
+
+import cats.data.Validated._
+import cats.data.Validated
+
+import org.scalacheck.{Arbitrary, Gen}
+
 /**
   *
   */
 object ArbitraryADTs {
+
+     implicit def arbIdentity[A: Arbitrary]: Arbitrary[Identity[A]] ={
+
+          val genIdentity: Gen[Identity[A]] = for {
+               a <- Arbitrary.arbitrary[A]
+          } yield Identity(a)
+
+          Arbitrary(genIdentity)
+     }
+
 
 }
