a99eaac22df2cd74368d6495f56a3423f19aeddf
statisticallyfit <statisticallyfit@gmail.com>
2021-09-23 16:26:50 +0300

    finished pawel szulc examples

--- src/test/scala/tutorialtests/PawelSzulc_GoingBananasWithRecursionSchemes/ExpWithAnaCataHyloProp.scala
+++ src/test/scala/tutorialtests/PawelSzulc_GoingBananasWithRecursionSchemes/ExpWithAnaCataHyloProp.scala
@@ -1,55 +1,85 @@
 package tutorialtests.PawelSzulc_GoingBananasWithRecursionSchemes
 
-/**
- *
- */
-//import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
-//import org.specs2.mutable._
-//import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-import org.scalatest._
 
-import org.scalatest.prop._
-import org.scalatest.propspec.AnyPropSpec
-import org.scalatest.funspec.AnyFunSpec
-import org.scalatest.matchers._
+
+
+//import org.scalatest.funspec.AnyFunSpec
+/*import org.scalatest.matchers.should
 import org.scalatest.{BeforeAndAfterAll/*, FlatSpec*/} //TODO where is FlatSpec? why not found?
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks*/
 
-import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-//import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import org.scalacheck._
 import org.scalacheck.Prop.forAll
-import org.scalacheck.{Arbitrary, Gen, Prop}
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalacheck.Arbitrary.arbitrary
 
 import scala.language.higherKinds
-import matryoshka._
+
 
 // NOTE: need this to avoid error of "No implicits found for Corecursive[T]" when doing anamorphism. MEANING: Fix needs to implement BirecursiveT typeclass
 import matryoshka.data.Fix.birecursiveT
+import matryoshka._
 import matryoshka.data._
 import matryoshka.implicits._
-
 
 import RecursionSchemeTutorials.PawelSzulc_GoingBananasWithRecursionSchemes.Part5_AnaCoalgebra._
 import RecursionSchemeTutorials.PawelSzulc_GoingBananasWithRecursionSchemes.Part5_AnaCoalgebra.ExpOps._
 import RecursionSchemeTutorials.PawelSzulc_GoingBananasWithRecursionSchemes.Part5_AnaCoalgebra.ExpFunctor._
+
+
+import scalaz.Functor
+import scalaz.syntax.all._
 
 /**
  *
  */
 
 //trait A extends AnyFunSpec
+/*
+trait Spec extends AnyFunSpec with BeforeAndAfterAll with ScalaCheckDrivenPropertyChecks  with should.Matchers*/
 
-trait Spec extends AnyFunSpec with /*BeforeAndAfterAll with*/ ScalaCheckDrivenPropertyChecks  with should.Matchers
 
-class DivisorLaws extends Properties("Divisor Spec") with Spec {
+class SpecClass extends Properties("Divisor Spec") /*with Spec*/ {
 
-	val positiveInt: Gen[Int] = arbitrary[Int] suchThat (_ > 0) // avoid division by zero error here?
 
-	val divisorsToInitialValueByAnaThenCata = forAll(positiveInt) { (number: Int) =>
-		number.ana[Fix[Exp]](divisors).cata(evaluate) shouldEqual number
+	trait DivisorLaws[F[_]] {
+
+		implicit def functor: Functor[F]
+
+		val positiveInt: Gen[Int] = arbitrary[Int] suchThat (_ > 0) // avoid division by zero error here?
+
+		def divisorsToInitialValueByAnaThenCata = {
+			property("ana then cata should evaluate divisors back to initial value") =
+				forAll(positiveInt) { (number: Int) =>
+					number.ana[Fix[Exp]](divisors).cata(evaluate) == number
+				}
+		}
+
+		def divisorsToInitialValueByHylo = {
+			property("hylo should evaluate divisors back to initial value") =
+				forAll(positiveInt) { (number: Int) =>
+					number.hylo[Exp, Double](evaluate, divisors) == number
+				}
+		}
+
+		def divisorsToInitialValueEquivHyloAndAnaCata = {
+			property("ana then cata is same as hylo for to evaluate divisors back to initial value") =
+				forAll(positiveInt) { (number: Int) =>
+					number.ana[Fix[Exp]](divisors).cata(evaluate) ==
+						number.hylo[Exp, Double](evaluate, divisors)
+				}
+		}
+
+		// TODO how to test catamorphism? (how to create arbitrary of Fix[Exp]? and then how to evaluate before the
+		//  test to check the number the test results in?
+	}
+
+	object DivisorLaws {
+		def apply[F[_]](implicit ev: Functor[F]): DivisorLaws[F] = new DivisorLaws[F] {
+			def functor: Functor[F] = ev
+		}
 	}
 	/*val divisorsToInitialValueByAnaThenCata = describe("divisors") {
 		it("ana then cata should evaluate divisors back to initial value") {
@@ -59,7 +89,26 @@ class DivisorLaws extends Properties("Divisor Spec") with Spec {
 		}
 	}*/
 
-	val divisorsToInitialValueByHylo = describe("divisors") {
+	/*describe("divisors"){
+		/*it("ana then cata should evaluate divisors back to initial value") {
+			val divisorsToInitialValueByAnaThenCata = forAll(positiveInt) { (number: Int) =>
+				number.ana[Fix[Exp]](divisors).cata(evaluate) shouldEqual number
+			}
+		}*/
+		it("hylo should evaluate divisors back to initial value") {
+			val divisorsToInitialValueByHylo = forAll(positiveInt) { (number: Int) =>
+				number.hylo[Exp, Double](evaluate, divisors) shouldEqual number
+			}
+		}
+		it("ana then cata is same as hylo for to evaluate divisors back to initial value"){
+			val divisorsToInitialValueEquivHyloAndAnaCata = forAll(positiveInt) { (number: Int) =>
+				number.ana[Fix[Exp]](divisors).cata(evaluate) shouldEqual
+					number.hylo[Exp, Double](evaluate, divisors)
+			}
+		}
+	}*/
+
+	/*val divisorsToInitialValueByHylo = describe("divisors") {
 		it("hylo should evaluate divisors back to initial value") {
 			forAll(positiveInt) { (number: Int) =>
 				number.hylo[Exp, Double](evaluate, divisors) shouldEqual number
@@ -75,21 +124,16 @@ class DivisorLaws extends Properties("Divisor Spec") with Spec {
 					number.hylo[Exp, Double](evaluate, divisors)
 			}
 		}
-	}
+	}*/
 
 }
 
 
 
 
-object ExpWithAnaCataHyloProp  {
+object ExpWithAnaCataHyloProp extends SpecClass {
 
-	val d = new DivisorLaws
-	//d.divisorsToInitialValueByAnaThenCata.check
-	// TODO how to call these things? want to make it like those in functortests but cannot since apply method
-	//  doesn't take those kinds of parameters
-
-	/*DivisorLaws.divisorsToInitialValueByAnaThenCata
-	DivisorLaws.divisorsToInitialValueByHylo
-	DivisorLaws.divisorsToInitialValueEquivHyloAndAnaCata*/
+	DivisorLaws[Exp].divisorsToInitialValueByAnaThenCata
+	DivisorLaws[Exp].divisorsToInitialValueByHylo
+	DivisorLaws[Exp].divisorsToInitialValueEquivHyloAndAnaCata
 }
